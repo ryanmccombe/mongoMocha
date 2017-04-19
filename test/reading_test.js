c1b718cfa5616a427c70b4c5df0f16d28b4c0845
Ryan McCombe <ryanmccombe@gmail.com>
2017-04-19 02:56:42 +0100

    Sort Skip Limit

--- test/reading_test.js
+++ test/reading_test.js
@@ -3,11 +3,20 @@ const User = require('../src/user');
 
 describe('Reading users out of the database', () => {
     'use strict'; // Block-scoped declarations not yet supported outside strict mode
-    let joe;
+    let joe, alex, maria, zara;
 
     beforeEach((done) => {
+        alex = new User({ name: 'Alex'});
         joe = new User({ name: 'Joe' });
-        joe.save().then(() => done());
+        maria = new User({ name: 'Maria' });
+        zara = new User({ name: 'Zara' });
+
+        Promise.all([
+            alex.save(),
+            joe.save(),
+            maria.save(),
+            zara.save()
+        ]).then(() => done())
     });
 
     it('for all users with a name of Joe', (done) => {
@@ -23,5 +32,18 @@ describe('Reading users out of the database', () => {
             assert(user.name === 'Joe');
             done();
         });
-    })
+    });
+
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort({ name: 1 })
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                assert(users.length === 2);
+                assert(users[0].name === 'Joe');
+                assert(users[1].name === 'Maria');
+                done();
+            });
+    });
 });
